b3270f90e75c1ce8bc71ed816b747e642b457166
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-14 11:43:18 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,25 @@
-require 'open-uri'
 require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+    doc = Nokogiri::HTML(open(index_url))
+    cards = doc.css(".roster-cards-container")
+    cards.each do |card|
+      card.css(".student-card a").each do |student|
+        name = cards.css(".student-name").text
+        city = cards.css(".student-location").text
+        profile_url = student.attr("href")
+        students << {:name => name, :location => city, :profile_url => profile_url}
+    end
+  end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
