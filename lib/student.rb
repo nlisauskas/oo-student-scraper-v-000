080dc05d4ef3eb0b0c7b191d50eca6ae7e6fe49a
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-14 12:22:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
     @profile_quote = student_hash[:profile_quote]
     @bio = student_hash[:bio]
     @profile_url = student_hash[:profile_url]
-    @@all << student_hash
+    @@all << student_hash[:name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url]
   end
 
   def self.create_from_collection(students_array)
